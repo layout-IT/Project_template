bc642935121abadde689e9a5067222ca4ba06ffe
layout-IT <77637040+layout-IT@users.noreply.github.com>
2022-03-29 14:49:16 +0300

    fixed an error in index

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
-import { STORE } from 'redux';
 
 import reportWebVitals from './reportWebVitals';
 
 import { App } from 'App';
+import { STORE } from 'redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
